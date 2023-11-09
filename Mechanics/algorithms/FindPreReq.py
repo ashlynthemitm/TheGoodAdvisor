b8c7d5046dcbd9203edc5d510d252362734b0ee9
ashlynthemitm <ashlyncmitm@gmail.com>
2023-11-09 01:58:15 -0500

    findPrereqs and data is stored

--- Mechanics/algorithms/FindPreReq.py
+++ Mechanics/algorithms/FindPreReq.py
@@ -13,31 +13,16 @@ Algorithm mechanics --> prioritize type first (CORE > MAJOR > CERTIFICATE)
 
 place previous taken courses in set --> compare set to any courses planning to add to queue
 '''
+import mysql.connector
+from dotenv import load_dotenv
+import os 
 
-# Utilize course objects to view previous instances
-# if pre_req set is fufilled with Course.name in other categories the student is fine
-class Course:
-    def __init__(self, id, course_type, course_area, course_code, course_title, credit_hours, pre_reqs):
-        self.id = id
-        self.course_type = course_type
-        self.course_area = course_area
-        self.course_code = course_code
-        self.course_title = course_title
-        self.credit_hours = credit_hours
-        self.pre_reqs = self.createPreReqList(pre_reqs) # A 2d list containing prereqs
-        
-    def createPreReqList(self):
-        prlist = self.pre_reqs.split(',')
-        # Ex. [MATH 3302 or MATH 1111, CSC 1301]
-        for index, value in enumerate(prlist):
-            if 'or' in value:
-                prlist[index] = value.split(' or ')
-                    
-        return prlist
 
-def GetPrevCoursesInfo():
-    return "SELECT * FROM CSCMajorCourses"
-        
+def findCoursePrerequsite(course):
+    '''
+    if a user requests the prerequsites for a course this function will output that data
+    '''
+
 def main():
     load_dotenv() # creation of .env file to store mysql credentials 
     host_db = os.getenv('DB_HOST')
@@ -49,21 +34,26 @@ def main():
         password=password_db
     )
     
-    # using this cursor to input data
-    cursor = TheGoodAdvisor_db.cursor() 
-    
-    # select columns from the database
-    course_info_query = GetPrevCoursesInfo()
-    cursor.execute(course_info_query)
-    
-    # store the course info in a variable
-    course_info = cursor.fetchall() # [(id, course_type,, course_area,, course_code,, course_title, credit_hours,, pre_reqs)]
+    try: 
         
-    # commit the changes
-    TheGoodAdvisor_db.commit()
+        # using this cursor to input data
+        cursor = TheGoodAdvisor_db.cursor() 
+        cursor.execute('use thegoodadvisordb;')
+
+        # input data
+            
+        # commit the changes
+        TheGoodAdvisor_db.commit()
+        
+    except mysql.connector.Error as error:
+        # Handle errors
+        print(f'Error: {error}')
     
-    # close the connection
-    TheGoodAdvisor_db.close()
+    finally:
+        # close the cursor and database connection
+        cursor.close()
+        TheGoodAdvisor_db.close()
+        
 
 if __name__ == "__main__":
     main()
--- Mechanics/algorithms/FourYearPlan.py
+++ Mechanics/algorithms/FourYearPlan.py
@@ -1,6 +1,18 @@
 '''
 Four Year Plan Algorithm based on previous input of previous coursework or 0 credits.
-
-
 Author: Ashlyn Campbell 
 '''
+
+class DeterminePlan():
+    def __init__(self):
+        self.priority_stack = ['Core Curriculum', 'Major Requirements', 'Certificate']
+        self.completed_courses = set()
+
+
+def main():
+    '''
+    add database connection
+    '''
+    
+if __name__ == '__main__':
+    main()
