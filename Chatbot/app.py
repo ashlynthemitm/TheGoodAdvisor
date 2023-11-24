f510a01fb7e000982b9a3baefffd8ec86c6f4363
Victor Wilberforce <victorwilberforce2310@gmail.com>
2023-11-23 19:37:57 -0500

    working on app route

--- Chatbot/app.py
+++ Chatbot/app.py
@@ -1,6 +1,16 @@
 from flask import Flask, request, jsonify
+from Mechanics.algorithms import ChatAlgorithm
+from Mechanics.algorithms import ChatOutput
 
 app = Flask(__name__)
+
+@app.route('/find-prerequisite', methods=['POST'])
+def find_prerequisite():
+    data = request.json
+    requested_course = data.get('request_course')
+
+    
+    
 
 if __name__=='__main__':
     app.run(debug=True)
