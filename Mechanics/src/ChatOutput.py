78bfe66b557f30a2692acac9e84ce200f920d36e
ashlynthemitm <ashlyncmitm@gmail.com>
2023-11-26 23:26:31 -0500

    More updates are needed but the actual connection is established now and fourYear Plan general pathway works

--- Mechanics/src/ChatOutput.py
+++ Mechanics/src/ChatOutput.py
@@ -20,9 +20,10 @@ class ChatOutput:
             print('Error no selection has been made to find output!!!')
             return
         
-        self.output = f""" I am happy to help you with your course needs! I see that you have selected to {requestType} to assist with your college journey! \n I have attached below the requested information!\n\n"""
+        self.output = self.output = f'<p>I am happy to help you with your course needs! I see that you have selected to {requestType} to assist with your college journey!<br><br>I have attached below the requested information!<br><br></p>'
+
         
-    def findPrerequisite(self, completed_courses=[], find_prerequisites=[True], create_four_year_plan=False, isDataScience=False, isCYBER=False, isSWE=False):
+    def findPrerequisite(self, completed_courses=[], find_prerequisites=[], create_four_year_plan=False, isDataScience=False, isCYBER=False, isSWE=False):
         # returns a dictionary of prereqs
         course_prerequisites = ChatAlgorithm.main(completed_courses=completed_courses, find_prerequisites=find_prerequisites, create_four_year_plan=False, isDataScience=False, isCYBER=False, isSWE=False)
         
@@ -42,12 +43,12 @@ class ChatOutput:
         four_year_plan = ChatAlgorithm.main(completed_courses=completed_courses, find_prerequisites=find_prerequisites, create_four_year_plan=True, isDataScience=True, isCYBER=False, isSWE=True)
         # returns a 2d array representing the four-year plan
         
-        self.output += f'The Four-Year Plan for your anticipated graduation is\n'
+        self.output += f'<p>The Four-Year Plan for your anticipated graduation is<br>'
         for year in four_year_plan.keys():
             first_semester = ', '.join(map(str, four_year_plan[year][0]))
             second_semester = ', '.join(map(str, four_year_plan[year][1]))
-            self.output += f"""{year} - \n Semester 1: {first_semester} \n Semester 2: {second_semester} \n"""
-        self.output += '\n I hope this four-year plan suits your interests!'
+            self.output += f'{year} - <br>Semester 1: {first_semester} <br> Semester 2: {second_semester} <br>'
+        self.output += '<br> I hope this four-year plan suits your interests!</p>'
         
         return self.output
     
